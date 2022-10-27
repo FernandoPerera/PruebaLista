f0d73e4a12be172f7232d30c3c4860638f7f186d
Fernando <you@example.com>
2022-10-27 21:48:25 +0200

    Feature(App.js): Realizada la gestión de espacio que tendrá la aplicación)

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <View style={styles.productSpace}>
+
+        <Text>Product Input</Text>
+
+      </View>
+
+      <View style={styles.product}>
+
+        <Text>List Item</Text>
+
+      </View>
+
+      <Pressable style={styles.clearContainer}>
+
+        <View style={styles.clearText}>
+
+          <Text>Clear</Text>
+
+        </View>
+
+      </Pressable>
+
+
     </View>
   );
 }
@@ -16,5 +37,37 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },  
+  product : {
+    flex : 3, 
+    width: '90%',
+    borderWidth: 2,
+    borderRadius: 12,
+    backgroundColor: '#E8B775',
+    margin: 10,
+    marginBottom: 30,
+    padding: 10,
   },
+  productSpace : {
+    flex:1,
+    alignContent: 'center',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#77B80A',
+    width: '80%',
+    height: 80,
+    borderRadius: 10,
+    marginTop: 45
+  },
+  clearContainer: {
+    alignItems: 'center',
+    flex: 0.5,
+  },
+  clearText: {
+    padding: 15,
+    width: '90%',
+    borderWidth: 2,
+    borderRadius: 12,
+    backgroundColor: '#E8B775'
+  }, 
 });
